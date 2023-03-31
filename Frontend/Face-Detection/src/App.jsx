b62459c610373ed5390d8b7adace8c67c8210fe3
Eduardo Tun <eduardotun27@gmail.com>
2023-03-31 11:45:29 -0600

    deteccion de la camara en el navegador

--- Frontend/Face-Detection/src/App.jsx
+++ Frontend/Face-Detection/src/App.jsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
+import { useState,useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+import {  Modal } from "antd";
+import Webcam from 'react-webcam';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const webcamRef = useRef(null);
+
+  const [isCameraVisible, setIsCameraVisible] = useState(false);
+
+  const handleCameraClick = () => {
+    setIsCameraVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsCameraVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsCameraVisible(false);
+  };
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    onCapture(imageSrc);
+  };
+ 
+
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Form
+    name="login-form"
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    style={{ maxWidth: 400, margin: 'auto' }}
+  >
+    <Form.Item
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input placeholder="Username" />
+    </Form.Item>
+
+    <Form.Item
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password placeholder="Password" />
+    </Form.Item>
+
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Log in
+      </Button>
+    </Form.Item>
+
+    <Form.Item>
+      <Button type="primary" onClick={handleCameraClick}>
+        Activate camera
+      </Button>
+      <Modal open={isCameraVisible} onCancel={handleCancel} footer={null}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          style={{ width: '100%', maxWidth: '500px' }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+        <Button onClick={capture}>Capture</Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </Modal>
+     
+    </Form.Item>
+  </Form>
   )
 }
 
